b01072b93b4b952adf7b2ad3cebf9f241c51f2b9
Pedro Oliveira <pedro.ott@hotmail.com>
2020-04-16 15:03:30 -0300

    guess game 4.0

--- 1_intro_to_py/guess_game.py
+++ 1_intro_to_py/guess_game.py
@@ -1,14 +1,25 @@
+import random;
+
 print("\n********************************");
 print("Bem vindo ao jogo da adivinhação");
 print("********************************\n");
 
-secret_number = 42;
-tries = 3;
-run = 1;
+secret_number = random.randrange(1,101);
+tries = 0;
+points = 1000;
+print("Defina o nível da dificuldade");
+level = int(input("(1) Fácil (2) Médio (3) Difícil\n->"));
+
+if level == 1:
+    tries = 20;
+elif level == 2:
+    tries = 10;
+else:
+    tries = 5;
 
 for run in range(tries):
-    print("\nTentativa", run, "de", tries);
-    guess_str = input("Qual seu chute: \n-> ");
+    print("\nTentativa {} de {}".format(run+1, tries));
+    guess_str = input("Qual seu chute (Entre 1 e 100): \n-> ");
     guess = int(guess_str);
 
     correct = guess == secret_number;
@@ -17,10 +28,12 @@ for run in range(tries):
 
     if guess < 1 or guess > 100:
         print("Você deve digitar um número entre 1 e 100!");
+        points -= abs(secret_number - 100);
         continue
 
     if correct:
         print("Acertou");
+        points += abs(level*(run-tries))
         break;
     else:
         if bigger:
@@ -29,5 +42,7 @@ for run in range(tries):
         elif smaller:
 
             print("Errou! O chute foi menor que o número secreto");
+        points -= abs(secret_number - guess);
 
+print(f'Pontuação: {points}');
 print("Fim de jogo");
