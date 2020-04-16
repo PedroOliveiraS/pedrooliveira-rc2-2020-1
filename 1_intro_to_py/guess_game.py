f5504de9c4c606e010da8c4f9f18dcf5ae0a42f9
Pedro Oliveira <pedro.ott@hotmail.com>
2020-04-16 14:47:44 -0300

    guess game 3.0

--- 1_intro_to_py/guess_game.py
+++ 1_intro_to_py/guess_game.py
@@ -6,7 +6,7 @@ secret_number = 42;
 tries = 3;
 run = 1;
 
-while tries:
+for run in range(tries):
     print("\nTentativa", run, "de", tries);
     guess_str = input("Qual seu chute: \n-> ");
     guess = int(guess_str);
@@ -15,14 +15,19 @@ while tries:
     bigger = guess > secret_number;
     smaller = guess < secret_number;
 
+    if guess < 1 or guess > 100:
+        print("Você deve digitar um número entre 1 e 100!");
+        continue
+
     if correct:
         print("Acertou");
+        break;
     else:
         if bigger:
             print("Errou! O chute foi maior que o número secreto");
+
         elif smaller:
+
             print("Errou! O chute foi menor que o número secreto");
-    run+=1;
-    tries-=1;
 
 print("Fim de jogo");
