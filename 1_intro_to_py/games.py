248d9504ba91b64db6647585d2f762b0dd92c1d2
Pedro Oliveira <pedro.ott@hotmail.com>
2020-04-23 15:04:13 -0300

    games1.0

---
+++ 1_intro_to_py/games.py
@@ -0,0 +1,19 @@
+import hangmman_game
+import guess_game
+
+def choose_game():
+    print("*********************************")
+    print("*******Escolha o seu jogo!*******")
+    print("*********************************")
+
+    game = int(input("(1) Forca (2) Adivinhação"))
+
+    if game == 1:
+        print("Jogando Forca")
+        hangmman_game.play()
+    else:
+        print("Jogando Adivinhação")
+        guess_game.play()
+
+if __name__ == "__main__":
+    choose_game()
---
+++ 1_intro_to_py/hangman_game.py
@@ -0,0 +1,8 @@
+def play():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+
+if __name__ == "__main__":
+    play();
