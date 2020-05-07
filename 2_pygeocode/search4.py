8b0812ba28c398adfff6caa86eeeb712a76c7737
Pedro Oliveira <pedro.ott@hotmail.com>
2020-05-06 22:24:16 -0300

    Search4 1.35

--- 2_pygeocode/search4.py
+++ 2_pygeocode/search4.py
@@ -24,7 +24,25 @@ def geocode(address):
         if not more:
             break
         raw_reply += more
-    print(raw_reply.decode('utf-8'))
+    newraw = raw_reply.decode('utf-8')
+    print(newraw)
+    print("MEU DEUS DO CEU")
+
+    print(type(newraw))
+    altered = newraw.split('\n')
+    tam = len(altered)
+    dec = altered[tam-4]
+    print(dec)
+    #print(dec.find('lon'))
+    #print(dec[235:300])
+    #novo = dec[234:300]
+    #print(novo)
+    #print(dec['place_id'])
+
+   # for i in range(len(raw_reply)):
+   #     print(f"Resultado {i}:")
+   #     print("     CEP: {}".format(raw_reply[i]['display_name'].split(",")[-2]))
+   #     print("     (Latitude, Longitude): ({})".format(raw_reply[i]['lat'], raw_reply[i]['lon']))
 
 
 if __name__ == '__main__':
